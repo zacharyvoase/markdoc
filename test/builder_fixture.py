b548c8c99be93da92f87ee08565b0113e6fb557b
Zachary Voase <zacharyvoase@me.com>
2009-11-12 23:38:03 +0100

    Split the builder doctest into two, so we can now use proper fixtures.

---
+++ test/builder_fixture.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os.path as p
+
+from common import get_temporary_config, clean_temporary_config
+
+
+def setup_test(test):
+    test.globs['CONFIG'] = get_temporary_config()
+    test.globs['CONFIG_DIR'] = p.join(test.globs['CONFIG']['meta']['root'], '')
+
+
+def teardown_test(test):
+    clean_temporary_config(test.globs['CONFIG'])
