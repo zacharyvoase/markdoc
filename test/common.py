431fd6ed68d949cfb987269baccad3b819e79b83
Zachary Voase <zacharyvoase@me.com>
2009-11-15 00:29:18 +0100

    Changed test/common.py to use the example Markdoc wiki.

--- test/common.py
+++ test/common.py
@@ -12,13 +12,13 @@ def get_temporary_config():
     """
     Return a temporary Markdoc configuration.
     
-    The contents of the wiki will be copied from Markdoc's own documentation.
-    After you're done with this, you should call `clean_temporary_config()` on
-    the config object.
+    The contents of the wiki will be copied from the example Markdoc wiki. After
+    you're done with this, you should call `clean_temporary_config()` on the
+    config object.
     """
     
-    own_config_dir = p.join(p.dirname(p.dirname(p.abspath(__file__))), 'doc') + p.sep
-    temp_config_dir = p.join(tempfile.mkdtemp(), 'doc')
+    own_config_dir = p.join(p.dirname(p.abspath(__file__)), 'example') + p.sep
+    temp_config_dir = p.join(tempfile.mkdtemp(), 'example')
     shutil.copytree(own_config_dir, temp_config_dir)
     return Config.for_directory(temp_config_dir)
 
