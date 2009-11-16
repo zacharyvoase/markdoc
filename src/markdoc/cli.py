b65bf2f49ba9160fd3fd29552194fac6450a6b38
Zachary Voase <zacharyvoase@me.com>
2009-11-16 13:04:08 +0100

    Added some more to cli.py. This will probably get split into a sub-module soon.

--- src/markdoc/cli.py
+++ src/markdoc/cli.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import argparse
 
 
+parser = argparse.ArgumentParser(**{
+    'prog': 'markdoc',
+    'description': 'A Markdown-based documentation build tool',
+    'version': '1.0'
+})
+
+parser.add_argument('--config', '-c', default=os.getcwd())
+
+subparsers = parser.add_subparsers(dest='command')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    # TODO: implement task runner.
