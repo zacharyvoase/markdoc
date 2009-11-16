5d17ea70929c4d4047cc6ed9dcea176ef32ee527
Zachary Voase <zacharyvoase@me.com>
2009-11-16 13:44:25 +0100

    Added some useful tasks machinery. More tasks to come.

--- src/markdoc/cli/tasks.py
+++ src/markdoc/cli/tasks.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import pprint
+
+from markdoc.cli.parser import subparsers
+
+
+def command(function):
+    """Decorator to declare a function as a Markdoc CLI task."""
+    
+    function.parser = subparsers.add_parser(
+        function.__name__.replace('_', '-'),
+        help=function.__doc__.rstrip('.'))
+    return function
+
+
+@command
+def show_config(config, args):
+    """Pretty-print the current Markdoc configuration."""
+    
+    pprint.pprint(config)
