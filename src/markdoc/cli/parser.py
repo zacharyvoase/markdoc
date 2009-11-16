d4be994246d336d8a52d2ceb0e8f6b44dc69dfa0
Zachary Voase <zacharyvoase@me.com>
2009-11-16 15:57:53 +0100

    Made --quiet a global CLI option.

--- src/markdoc/cli/parser.py
+++ src/markdoc/cli/parser.py
@@ -17,5 +17,8 @@ config = parser.add_argument('--config', '-c', default=os.getcwd(),
     help="Use the specified Markdoc config (a YAML file or a directory "
          "containing markdoc.yaml)")
 
+quiet = parser.add_argument('--quiet', '-q', default=False, action='store_true',
+    help="Suppress non-error output")
+
 subparsers = parser.add_subparsers(dest='command', title='commands', metavar='CMD')
 
