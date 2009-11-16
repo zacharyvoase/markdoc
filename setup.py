957e0008bb89594e67ee19b9d536a99355943453
Zachary Voase <zacharyvoase@me.com>
2009-11-16 20:51:06 +0100

    Added some workarounds for Python < 2.6, namely os.path.relpath().

--- setup.py
+++ setup.py
@@ -5,6 +5,26 @@ import os
 import re
 
 
+if not hasattr(os.path, 'relpath'):
+    def relpath(path, start=os.path.curdir):
+        """Return a relative version of a path"""
+        
+        if not path:
+            raise ValueError("no path specified")
+        
+        start_list = os.path.abspath(start).split(os.path.sep)
+        path_list = os.path.abspath(path).split(os.path.sep)
+        
+        # Work out how much of the filepath is shared by start and path.
+        i = len(os.path.commonprefix([start_list, path_list]))
+        
+        rel_list = [os.path.pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return os.path.curdir
+        return os.path.join(*rel_list)
+    os.path.relpath = relpath
+
+
 def get_version():
     filename = os.path.join(os.path.dirname(__file__),
         'src', 'markdoc', '__init__.py')
--- src/markdoc/__init__.py
+++ src/markdoc/__init__.py
@@ -5,3 +5,22 @@ import os
 
 __version__ = '0.1'
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
+
+if not hasattr(os.path, 'relpath'):
+    def relpath(path, start=os.path.curdir):
+        """Return a relative version of a path"""
+        
+        if not path:
+            raise ValueError("no path specified")
+        
+        start_list = os.path.abspath(start).split(os.path.sep)
+        path_list = os.path.abspath(path).split(os.path.sep)
+        
+        # Work out how much of the filepath is shared by start and path.
+        i = len(os.path.commonprefix([start_list, path_list]))
+        
+        rel_list = [os.path.pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return os.path.curdir
+        return os.path.join(*rel_list)
+    os.path.relpath = relpath
