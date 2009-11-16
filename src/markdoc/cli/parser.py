845b1667281fab4d8be671fbc62ae096f26819e8
Zachary Voase <zacharyvoase@me.com>
2009-11-16 13:40:44 +0100

    Split the parser off into markdoc.cli.parser.

---
+++ src/markdoc/cli/parser.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import argparse
+
+from markdoc.config import Config
+
+
+parser = argparse.ArgumentParser(**{
+    'prog': 'markdoc',
+    'description': 'A Markdown-based documentation build tool',
+    'version': '1.0'
+})
+
+config = parser.add_argument('--config', '-c', default=os.getcwd(),
+    help="Use the specified Markdoc config (a YAML file or a directory "
+         "containing markdoc.yaml)")
+
+subparsers = parser.add_subparsers(dest='command', title='commands', metavar='CMD')
