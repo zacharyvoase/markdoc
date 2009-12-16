8fa0b7b267c6bf5d143f68be445db77a6dd6870d
Zachary Voase <zacharyvoase@me.com>
2009-12-16 20:03:26 +0100

    Bumped to v0.4.2.

--- src/markdoc/__init__.py
+++ src/markdoc/__init__.py
@@ -5,7 +5,7 @@ import os
 import os.path as p
 
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 static_dir = p.join(p.dirname(__file__), 'static')
 default_static_dir = p.join(static_dir, 'default-static')
 default_template_dir = p.join(static_dir, 'default-templates')
